4c7417626ffb230580b3c166b5b5a20ef5fb6963
Nathan <nathansmota@gmail.com>
2024-11-04 03:48:47 -0300

    :sparkles: feat: creating Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,67 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { DollarSign, Users, CreditCard, Activity } from "lucide-react";
+import PageTitle from "@/components/PageTitle";
+import Card, { CardContent, CardProps } from "@/components/Card";
+import BarChart from "@/components/BarChart";
+import SalesCard, { SalesProps } from "@/components/SalesCard";
+import DashboardLayout from './DashboardLayout';
+
+const cardData: CardProps[] = [
+    {
+        label: "Receita Total",
+        amount: "R$45.231,89",
+        discription: "+20,1% em relação ao mês passado",
+        icon: DollarSign
+    },
+    {
+        label: "Assinaturas",
+        amount: "+2350",
+        discription: "+180,1% em relação ao mês passado",
+        icon: Users
+    },
+    {
+        label: "Vendas",
+        amount: "+12.234",
+        discription: "+19% em relação ao mês passado",
+        icon: CreditCard
+    },
+    {
+        label: "Ativos Agora",
+        amount: "+573",
+        discription: "+201 na última hora",
+        icon: Activity
+    }
+];
+
+const userSalesData: SalesProps[] = [
+    {
+        name: "Olivia Martin",
+        email: "olivia.martin@email.com",
+        saleAmount: "+R$1.999,00"
+    },
+    {
+        name: "Jackson Lee",
+        email: "jackson.lee@email.com",
+        saleAmount: "+R$1.999,00"
+    },
+    {
+        name: "Isabella Nguyen",
+        email: "isabella.nguyen@email.com",
+        saleAmount: "+R$39,00"
+    },
+    {
+        name: "William Kim",
+        email: "will@email.com",
+        saleAmount: "+R$299,00"
+    },
+    {
+        name: "Sofia Davis",
+        email: "sofia.davis@email.com",
+        saleAmount: "+R$39,00"
+    }
+];
 
 export default function Dashboard() {
     const router = useRouter();
@@ -23,6 +84,43 @@ export default function Dashboard() {
     }
 
     return (
-        <div>Bem-vindo ao Dashboard</div>
+        <DashboardLayout>
+            <div className="flex flex-col gap-5 w-full p-10">
+                <PageTitle title="Dashboard" />
+                <section className="grid w-full grid-cols-1 gap-4 gap-x-8 transition-all sm:grid-cols-2 xl:grid-cols-4">
+                    {cardData.map((d, i) => (
+                        <Card
+                            key={i}
+                            amount={d.amount}
+                            discription={d.discription}
+                            icon={d.icon}
+                            label={d.label}
+                        />
+                    ))}
+                </section>
+                <section className="grid grid-cols-1  gap-4 transition-all lg:grid-cols-2">
+                    <CardContent>
+                        <p className="p-4 font-semibold">Visão geral</p>
+                        <BarChart />
+                    </CardContent>
+                    <CardContent className="flex justify-between gap-4">
+                        <section>
+                            <p>Vendas Recentes</p>
+                            <p className="text-sm text-gray-400">
+                                Você fez 265 vendas nesse mês.
+                            </p>
+                        </section>
+                        {userSalesData.map((d, i) => (
+                            <SalesCard
+                                key={i}
+                                email={d.email}
+                                name={d.name}
+                                saleAmount={d.saleAmount}
+                            />
+                        ))}
+                    </CardContent>
+                </section>
+            </div>
+        </DashboardLayout>
     );
 }
